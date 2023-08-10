43d78484b29358b1835eca23982f8e8f2d346024
Manav <200170116034@vgec1.gujgov.edu.in>
2023-08-10 17:26:23 +0530

    whole project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
- 
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+
+import Searchbar from "./Components/Searchbar";
+import MyNavigation from "./MyNavigation";
+import store from "./State/store";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Provider store={store}>
+        <ToastContainer />
+        <Header />
+        <Searchbar />
+
+        <MyNavigation />
+        <Footer />
+      </Provider>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,40 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {NavLink,BrowserRouter,Routes,Route} from 'react-router-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Nav from './components/nav';
-import Login from './components/login';
-import Home from './components/home';
-import './App.css';
-import { ToastContainer } from "react-toastify";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
- import "react-toastify/dist/ReactToastify.css";
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<ToastContainer
-			position='top-right'
-			autoClose={5000}
-			hideProgressBar={false}
-			newestOnTop={false}
-			closeOnClick
-			rtl={false}
-			pauseOnFocusLoss
-			draggable
-			pauseOnHover
-			theme='light'
-		/>
-		<BrowserRouter>
-			<Nav />
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/login' element={<Login name='manav' />} />
-			</Routes>
-		</BrowserRouter>
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
